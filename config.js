b3220d8447eed4c97487226ba783dcf81b3ffad1
ShCoding-btc <genggetlu@gmail.com>
2026-01-15 15:00:33 +0800

    修改默认策略

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ export const tradingConfig = {
   // 订单配置
   order: {
     // 订单数量（用于创建买入和卖出订单买入卖出同数量）
-    quantity: '0.018',
+    quantity: '0.05',
     // 默认订单数量（用于 placeOrder 默认参数）
     defaultQuantity: '0.005',
     // 杠杆
@@ -22,10 +22,10 @@ export const tradingConfig = {
   // 价格调整配置
   price: {
     // 价格调整比例在下单时，以低于或高于当前市场价格的adjustmentRatio进行下单（0.088%）
-    adjustmentRatio: 0.0008,
+    adjustmentRatio: 0.00075,
     // 价格差值阈值（百分比）在下单时，如果价格差值小于min%或大于max%，则取消订单
     diffThreshold: {
-      min: 0.06,  // 最小差值阈值
+      min: 0.05,  // 最小差值阈值
       max: 0.1,  // 最大差值阈值
     },
   },
